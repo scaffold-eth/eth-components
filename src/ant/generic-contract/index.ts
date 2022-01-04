182fdada378821c4326ec9818e4977eb94cf1431
Bliss Da Sailor <meremezeukah@gmail.com>
2022-01-03 21:20:50 -0600

    clean up

--- src/ant/generic-contract/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './displayUtils';
-export * from './DisplayVariable';
-export * from './FunctionFrom';
-export * from './GenericContract';
-export * from './NoContractDisplay';
--- test/test-utils/functions/conversions.ts
+++
@@ -1,9 +0,0 @@
-import { BigNumber, utils } from 'ethers';
-
-export const fromGwei = (value: string | number): BigNumber => {
-  return utils.parseUnits(value.toString(), 'gwei');
-};
-
-export const fromEther = (value: string | number): BigNumber => {
-  return utils.parseEther(value.toString());
-};
